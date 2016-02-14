2ab7b6b9e916cdcdc43b4dc3703acf05ba44d2fd
rolstenhouse <rolsthoorn12@gmail.com>
2016-02-14 16:08:23 +0800

    Adding new changes

--- emails.php
+++ emails.php
@@ -16,22 +16,22 @@ if ($intervalUnit == 2){
 elseif($intervalUnit == 3){
 	$interval = $interval*3600;
 }
-$emails = array($phone . '@txt.att.net',
-	$phone . '@tmomail.net',
-	$phone . '@vtext.com',
-	$phone . '@messaging.sprintpcs.com', $phone . '@pm.sprint.com',
-	$phone . '@vmobl.com',
-	$phone . '@mmst5.tracfone.com',
-	$phone . '@mymetropcs.com',
-	$phone . '@myboostmobile.com',
-	$phone . '@sms.mycricket.com',
-	$phone . '@messaging.nextel.com',
-	$phone . '@message.alltel.com',
-	$phone . '@ptel.com',
-	$phone . '@tms.suncom.com',
-	$phone . '@qwestmp.com',
-	$phone . '@email.uscc.net',
-	);
+$emails = array($phone );//. '@txt.att.net',
+	// $phone . '@tmomail.net',
+	// $phone . '@vtext.com',
+	// $phone . '@messaging.sprintpcs.com', $phone . '@pm.sprint.com',
+	// $phone . '@vmobl.com',
+	// $phone . '@mmst5.tracfone.com',
+	// $phone . '@mymetropcs.com',
+	// $phone . '@myboostmobile.com',
+	// $phone . '@sms.mycricket.com',
+	// $phone . '@messaging.nextel.com',
+	// $phone . '@message.alltel.com',
+	// $phone . '@ptel.com',
+	// $phone . '@tms.suncom.com',
+	// $phone . '@qwestmp.com',
+	// $phone . '@email.uscc.net',
+	// );
 
 
 include_once 'facts.php';
@@ -46,7 +46,7 @@ foreach ($emails as $emailID) {
 	$mail->addAddress($emailID);
 
 	$mail->Subject = "Facts";
-	error_log("ASDF");
+	//error_log("ASDF");
 	$mail->Body ="You have been subscribed to Useless Facts by an enemy!\nThe message will occur ".$frequency." times, every ".$interval." seconds";
 
 	if(!$mail->send()) {
